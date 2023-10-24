9af193974b1fbd6a9e63d0af7545f33783092763
ossanaTomas <ossanatomas5@gmail.com>
2023-10-24 09:50:53 -0300

    funcionalidad de contar columas, va a servir para ver cantidad de depositos

--- main.cpp
+++ main.cpp
@@ -18,13 +18,15 @@ int main() {
 
     string linea;
     string dato;
+    string primerlinea;
     string Grupo, codigoBarras, articulo;
     // debemos ver como manejar la definicion de los depositos ya que estos son dinamicos.
-    int deposito1, deposito2, deposito3,deposito4 ,deposito5;
+    // int deposito1, deposito2, deposito3,deposito4 ,deposito5, ;
+    int contadordecolumnas;
     ArbolBinarioAVL<string> arbol1;
+    Lista<string> listaDecolunas;
 
     char delimitadorDeColumn = ',';
-    cout << "hola mundo funciona el cmake" << endl;
 
 
     ifstream inventario; // definimos un archivo
@@ -35,10 +37,21 @@ int main() {
     if (!inventario.is_open()) {
         cout << "error al abrir el archivo" << endl;
     }
-
     // vamos a leer la primera linea para saltar el encabezado.
-    getline(inventario,linea);
-    while (getline(inventario,linea)){ // mientras el archivo este abierto o no sea el final.
+    // y a la vez contamos la cantidad de columnas que vamos a tener.
+    getline(inventario, linea);
+    stringstream linea2;
+    linea2.str(linea);
+    {
+        while (getline(linea2, dato, delimitadorDeColumn)) { // aca contamos la cantida de columans
+            listaDecolunas.insertarUltimo(dato); // las guardamos en una lista por ahora.
+            contadordecolumnas++// sabemos que las primeras 3 siempre van a ser de grupo, cod y articulo, lo demas seran depositos.
+        }
+    };
+    cout << "cantidad de columnas son " << contadordecolumnas;
+
+
+    while (getline(inventario, linea)) { // mientras el archivo este abierto o no sea el final.
         // aca tambien podria decir  while ( !inventario.eof)
 
         stringstream stream(linea); // Convertir la cadena a un stream
@@ -46,11 +59,22 @@ int main() {
 
         // con este for, hago que los elementos que estan en la columna 1 me los meta en un arbol
         // podria hacer que me meta lo de otra columna en algun otra estructura.
-        for(int i=0; getline(stream,dato,delimitadorDeColumn); i++ ){
-            if(i==1){
+        for (int i = 0; getline(stream, dato, delimitadorDeColumn); i++) {
+
+            //0= grupo
+            //1= codigo de barras
+            //2= articulos
+            // todo lo demas deposito.
+
+            if (i == 1) {  //
                 arbol1.put(dato);
             }
         }
+
+
+    }
+    arbol1.inorder();
+}
 
 /*
         getline(stream, Grupo,',');
@@ -61,7 +85,25 @@ int main() {
         getline(stream, deposito3, ',');
         getline(stream,deposito4,',');
 */
+/*
+   int main(int argc,char **argv ){
+       string archivo= argev[argv-1];
+       cout<<"archivo"<<
 
-    }
-  arbol1.inorder();
-}
+
+       for(int i= 1; i<arg -1; i++){
+           string arg =argv[i];
+           cout<<arg<<endl;
+           if(arg=="hola"){
+               hola(archivo)
+           }
+           if(arg==" "){
+
+           }
+           if(arg==" "){
+
+           }
+       }
+   }
+*/
+
